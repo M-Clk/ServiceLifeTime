49f449f95ffa5b67265a217777a9abbe4dd29787
Muhammed Çelik <celikmuhammed21@hotmail.com>
2020-01-16 03:14:53 +0300

    Add project files.

---
+++ ServiceLifeTime/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ServiceLifeTime.Models;
+
+namespace ServiceLifeTime.Controllers
+{
+    
+    public class HomeController : Controller
+    {
+
+        private IUtilitySingleton _singleton;
+        private IUtilitySingleton _singleton2;
+
+        private IUtilityTransient _transient;
+        private IUtilityTransient _transient2;
+
+        private IUtilityScoped _scoped;
+        private IUtilityScoped _scoped2;
+
+        public HomeController(IUtilitySingleton singleton, IUtilitySingleton singleton2, IUtilityTransient transient, IUtilityTransient transient2, IUtilityScoped scoped, IUtilityScoped scoped2)
+        {
+            _singleton = singleton;
+            _singleton2 = singleton2;
+            _transient = transient;
+            _transient2 = transient2;
+            _scoped = scoped;
+            _scoped2 = scoped2;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.Singleton = _singleton.Key;
+            ViewBag.Singleton2 = _singleton2.Key;
+            
+            ViewBag.Transient = _transient.Key;
+            ViewBag.Transient2 = _transient2.Key;
+
+            ViewBag.Scoped = _scoped.Key;
+            ViewBag.Scoped2 = _scoped2.Key;
+
+            return View();
+        }
+    }
+}
---
+++ ServiceLifeTime/Models/Utilities.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServiceLifeTime.Models
+{
+    public interface IUtilitySingleton
+    {
+       string Key { get; set; }
+       string Name { get; set; }
+    }
+    public interface IUtilityTransient
+    {
+       string Key { get; set; }
+       string Name { get; set; }
+    }
+    public interface IUtilityScoped
+    {
+       string Key { get; set; }
+       string Name { get; set; }
+    }
+    class UtilitySingleton : IUtilitySingleton
+    {
+        public string Key { get; set; } = Guid.NewGuid().ToString();
+        public string Name { get; set; } = nameof(UtilitySingleton);
+    }
+    class UtilityTransient : IUtilityTransient
+    {
+        public string Key { get; set; } = Guid.NewGuid().ToString();
+        public string Name { get; set; } = nameof(UtilityTransient);
+    }
+    class UtilityScoped : IUtilityScoped
+    {
+        public string Key { get; set; } = Guid.NewGuid().ToString();
+        public string Name { get; set; } = nameof(UtilityScoped);
+    }
+}
